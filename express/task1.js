c0711cec22a98cb43f56472915cb5573d9c70439
zemedkunworkalem32 <workalemzame@gmail.com>
2026-02-09 13:49:59 +0300

    app was removed

--- express/task1.js
+++ express/task1.js
@@ -1,1 +1,25 @@
-import express from 'express';
+import express from 'express'
+const app = express();
+
+app.use(express.json());
+
+app.get('/home',(req,res)=>{
+  res.status(200).type("text/html").send('<h1 style="color:green;"> welcome to our home page!')
+});
+
+app.get('/about',(req,res)=>{
+  res.status(200).type("text/plain").send('welcome to our about page!');
+});
+
+app.get("/students",(req, res)=>{
+  const {studentId, name ,department, role} = req.query;
+  res.status(200).json({
+  id: studentId,
+  name,
+  department,
+  role
+  });
+});
+app.listen(3000,()=>{
+  console.log('server is listening on port 3000');
+});
