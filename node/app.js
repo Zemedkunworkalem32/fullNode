fd08b909d292a5613e60a895fb971c16d2633532
zemedkunworkalem32 <workalemzame@gmail.com>
2026-02-09 13:56:23 +0300

    updated files

--- node/app.js
+++ node/app.js
@@ -2,23 +2,16 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url === '/home') {
+  if (url === '/') {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Welcome to our Node.js Server!</h1>');
-  res.write('<p>This is a simple HTTP server built with Node.js.</p>');
-  res.write('<p>Feel free to explore and modify the code!</p>');
-  res.end();
+  res.end('welcome to home page');
   } else if (url === '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>About Us</h1>');
-    res.write('<p>We are a team of passionate developers building web applications.</p>');
-    res.end();
+    res.end('welcome to about page');
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.write('<h1>404 Not Found</h1>');
-    res.write('<p>The page you are looking for does not exist.</p>');
-    res.end();
-  }
+    res.end('page not found');
+  } 
 });
 
 server.listen(5000, () => {
