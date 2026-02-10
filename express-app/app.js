3e302975e829fd48a4c4c7bec3adea87545bcc8e
zemedkunworkalem32 <workalemzame@gmail.com>
2026-02-10 09:07:05 +0300

    update the port

--- express-app/app.js
+++ express-app/app.js
@@ -15,5 +15,5 @@ app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
